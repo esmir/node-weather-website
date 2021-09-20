056fb97384a00a8840b5d8315db78f7d92b57830
azer2021 <azerselimic@gmail.com>
2021-09-21 00:13:40 +0200

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,16 @@
+const request = require('request') 
+
+const forecast = (latitude, longtitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=8730c55865a7a9d55b867afc5925d653&query=' + latitude + ',' + longtitude
+    request({url, json: true }, (error, {body} ) => {
+        if (error) {
+            callback('Unable to connect ot weather service!', undefined)
+        } else if(body.error) {
+            callback('Unable to find location', undefined) 
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degres out. It is feel like ' + body.current.feelslike + ' degress out.' )
+        }   
+    })
+}
+
+module.exports = forecast; 
